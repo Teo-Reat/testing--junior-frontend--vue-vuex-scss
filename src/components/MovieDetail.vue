0653842ffd1f23cf9ca83ab52c22d01a146a23c2
Teo-Reat <teo.reat@gmail.com>
2021-01-05 15:32:54 +0300

    Base project v1.1

--- src/components/MovieDetail.vue
+++ src/components/MovieDetail.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="movie-detail-wrapper">
-    <div class="movie-detail home__movie movie" v-if="thisMovie">
+    <div class="movie-detail home__movie movie" v-if="movie">
       <div class="movie__image-wrapper">
-        <img class="movie__image" :src="thisMovie.poster" alt="Poster">
+        <img class="movie__image" :src="movie.poster" alt="Poster">
       </div>
       <div class="movie__text">
-        <div class="movie__text-time-wrapper" v-if="thisMovie.collapse.duration">
-          <span class="movie__text-time">{{ thisMovie.collapse.duration[0] }}</span>
+        <div class="movie__text-time-wrapper" v-if="movie.collapse.duration">
+          <span class="movie__text-time">{{ movie.collapse.duration[0] }}</span>
         </div>
-        <h2 class="movie__text-title">{{ thisMovie.title }}</h2>
+        <h2 class="movie__text-title">{{ movie.title }}</h2>
         <div class="movie__text-inner">
-          <span class="movie__text-year">{{ thisMovie.year }}, </span>
+          <span class="movie__text-year">{{ movie.year }}, </span>
           <span class="movie__text-genres">
-          <span class="movie__text-genre" v-for="(genre, key) in thisMovie.genres" :key="key">{{ genre }}, </span>
+          <span class="movie__text-genre" v-for="(genre, key) in movie.genres" :key="key">{{ genre }}, </span>
         </span>
         </div>
         <div class="movie__text-directors">
           <span class="movie__text-director-description">режиссёр: </span>
-          <span class="movie__text-director" v-for="(director, key) in thisMovie.directors" :key="key">{{ director }}</span>
+          <span class="movie__text-director" v-for="(director, key) in movie.directors" :key="key">{{ director }}</span>
         </div>
         <div class="movie__text-actors">
           <span class="movie__text-actor-description">Актёры: </span>
-          <span class="movie__text-actor" v-for="(actor, key) in thisMovie.actors" :key="key">{{ actor }}, </span>
+          <span class="movie__text-actor" v-for="(actor, key) in movie.actors" :key="key">{{ actor }}, </span>
         </div>
-        <div class="movie__text-description" v-if="thisMovie.description">{{ thisMovie.description }}</div>
+        <div class="movie__text-description" v-if="movie.description">{{ movie.description }}</div>
       </div>
     </div>
     <div class="about__no-id" v-else>
@@ -33,13 +33,14 @@
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+import axios from "axios"
 
 export default {
   name: "MovieDetail",
   data: () => {
     return {
-      // id: this.$route.params.id
+      movie: {}
     }
   },
 
@@ -49,12 +50,27 @@ export default {
     }
   },
 
-  computed: {
-    ...mapGetters(['getMovies']),
+  methods: {
+    ...mapActions(['fetchMovies']),
+
+    getMovieFromId() {
+      axios.get('https://floating-sierra-20135.herokuapp.com/api/movie/' + this.movieId)
+          .then(response => (this.movie = response.data.data))
+    },
 
     thisMovie: function () {
-      return this.getMovies.find(m => m.id === this.movieId)
-    }
+      let movie = this.getMovies.find(m => m.id === this.movieId)
+      if (movie) return this.movie = movie
+      this.getMovieFromId()
+    },
+  },
+
+  mounted() {
+    this.thisMovie()
+  },
+
+  computed: {
+    ...mapGetters(['getMovies']),
   }
 }
 </script>
